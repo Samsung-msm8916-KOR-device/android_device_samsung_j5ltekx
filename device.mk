ab66c4b078664c6425f62b1e665a8e8cf3d8a01e
David <davidmolinamorales1999@gmail.com>
2021-08-25 23:50:53 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, vendor/samsung/j5nlte/j5nlte-vendor.mk)
 
 # Inherit from common
-$(call inherit-product, device/samsung/msm8916-common/device-common.mk)
+$(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 
 LOCAL_PATH := device/samsung/j5nlte
 
