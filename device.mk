29335383386727388a50b4a40717e2fb629c2bc6
David <davidmolinamorales1999@gmail.com>
2021-09-02 13:10:18 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, vendor/samsung/j5nlte/j5nlte-vendor.mk)
 
 # Inherit J5 2015 device
-$(call inherit-product, device/samsung/msm8916-common/device.mk)
+$(call inherit-product, device/samsung/msm8916-common/device-j5.mk)
 
 # Inherit from common
 $(call inherit-product, device/samsung/j5-common/device-common.mk)
