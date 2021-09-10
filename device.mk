637b7934d31d2ff93308fd0aaaa1d5a1060e4ab9
David <davidmolinamorales1999@gmail.com>
2021-09-10 16:45:53 +0200

    j5nlte: changed path If needed

--- device.mk
+++ device.mk
@@ -20,7 +20,7 @@ $(call inherit-product, device/samsung/j5-common/device-common.mk)
 LOCAL_PATH := device/samsung/j5nlte
 
 # NFC
-$(call inherit-product, device/samsung/msm8916-common/j5-nfc.mk)
+$(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
